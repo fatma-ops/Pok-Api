0084d0759373f7c28a16cc64182f975c4824e79e
Enzo Daval <81628324+DavalEnzo@users.noreply.github.com>
2024-03-21 15:09:41 +0100

    Suppression pokedex (#2)
    * modif sur page erreur + ajout mourad super pokemon et pokedex
    
    * ajout suppression du pokédex entier + loader requête

---
+++ pokemon/src/routes/loading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Loading () {
+  return (
+    <div style={{height: '90vh'}} className={"d-flex justify-content-center align-items-center my-auto"}><img
+      style={{width: "8%"}} className={"u-bounce"}
+      src="https://www.freeiconspng.com/thumbs/pokeball-png/file-pokeball-png-0.png" alt="loader"/></div>
+  )
+}
