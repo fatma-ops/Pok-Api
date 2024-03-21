a863ab279bc5ee9624739080c3b65767ce7c462f
fatma-ops <73761826+fatma-ops@users.noreply.github.com>
2024-03-21 11:02:28 +0100

    pokemon list

---
+++ pokemon/src/pokemonList.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+ const PokemonList = () => {
+
+  const [pokemonDetails, setPokemonDetails] = useState(null);
+
+  useEffect(() => {
+    const fetchPokemonDetails = async () => {
+      try {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/`);
+        setPokemonDetails(response.data);
+      } catch (error) {
+        console.error('Error fetching pokemon details:', error);
+      }
+    };
+    fetchPokemonDetails();
+  });
+
+  if (!pokemonDetails) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h2>{pokemonDetails.name}</h2>
+      <p>Numéro: {pokemonDetails.id}</p>
+      <img  />
+      <button>Ajouter au Pokédex</button>
+    </div>
+  );
+};
+
+export default PokemonList;
