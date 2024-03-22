efbc108e8cb4464922f70ef41601a08539708aec
DavalEnzo <davalenzo@gmail.com>
2024-03-22 17:03:27 +0100

    indentation + résolution problème promesse

--- pokemon/src/routes/errorPage.jsx
+++ pokemon/src/routes/errorPage.jsx
@@ -1,31 +1,32 @@
-import React, { useEffect } from 'react';
-import { useRouteError, useNavigate } from "react-router-dom";
+import React, {useEffect} from 'react';
+import {useRouteError, useNavigate} from "react-router-dom";
 
 export default function ErrorPage() {
-    const error = useRouteError();
-    const navigate = useNavigate(); // Hook to programmatically navigate
+  const error = useRouteError();
+  const navigate = useNavigate(); // Hook to programmatically navigate
 
-    // Redirect user after a set time (e.g., 5000 milliseconds = 5 seconds)
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            navigate('/'); // Assuming '/' is the path to your list page
-        }, 5000); // Adjust the time as needed
+  // Redirect user after a set time (e.g., 5000 milliseconds = 5 seconds)
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/'); // Assuming '/' is the path to your list page
+    }, 5000); // Adjust the time as needed
 
-        // Cleanup function to clear the timer if the component unmounts before the timer fires
-        return () => clearTimeout(timer);
-    }, [navigate]);
+    // Cleanup function to clear the timer if the component unmounts before the timer fires
+    return () => clearTimeout(timer);
+  }, [navigate]);
 
-    return (
-        <div id="error-page" style={{height: '100vh'}} className={"d-flex flex-row justify-content-center align-items-center gap-4"}>
-            <img className={"rounded"} width={"15%"} src={'https://media1.tenor.com/m/_BiwWBWhYucAAAAd/what-huh.gif'}/>
-            <div className={"d-flex flex-column"}>
-                <h1>Mauvaise page!</h1>
-                <p>Hop là ! Il semblerait que vous ayez atterri sur une page qui n'existe pas</p>
-                <p>
-                    <p>Erreur: <i>{error.statusText || error.message}</i></p>
-                    <p>Vous serez redirigé vers la liste principale dans quelques secondes...</p>
-                </p>
-            </div>
-        </div>
-    );
+  return (
+    <div id="error-page" style={{height: '100vh'}}
+         className={"d-flex flex-row justify-content-center align-items-center gap-4"}>
+      <img className={"rounded"} width={"15%"} alt={"travolta huh ?"} src={'https://media1.tenor.com/m/_BiwWBWhYucAAAAd/what-huh.gif'}/>
+      <div className={"d-flex flex-column"}>
+        <h1>Mauvaise page!</h1>
+        <p>Hop là ! Il semblerait que vous ayez atterri sur une page qui n'existe pas</p>
+        <p>
+          <p>Erreur: <i>{error.statusText || error.message}</i></p>
+          <p>Vous serez redirigé vers la liste principale dans quelques secondes...</p>
+        </p>
+      </div>
+    </div>
+  );
 }
--- pokemon/src/routes/pokemonList.jsx
+++ pokemon/src/routes/pokemonList.jsx
@@ -7,121 +7,129 @@ import {faChevronLeft, faChevronRight} from "@fortawesome/free-solid-svg-icons";
 import {Link} from "react-router-dom";
 
 const PokemonList = () => {
-    const [allPokemon, setAllPokemon] = useState([]);
-    const [displayedPokemon, setDisplayedPokemon] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [currentPage, setCurrentPage] = useState(0);
-    const [itemsPerPage] = useState(40);
+  const [allPokemon, setAllPokemon] = useState([]);
+  const [displayedPokemon, setDisplayedPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [currentPage, setCurrentPage] = useState(0);
+  const [itemsPerPage] = useState(40);
 
-    useEffect(() => {
-        const fetchAllPokemon = async () => {
-            try {
-                const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1000');
-                setAllPokemon(response.data.results);
-            } catch (error) {
-                console.error('Error fetching all pokemon:', error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        const loading = setTimeout(()=>{
-            fetchAllPokemon();
-        }, 1500);
-
-        return () => clearTimeout(loading);
-    }, []);
-
-    useEffect(() => {
-        const fetchPokemonDetails = async (pokemonArray) => {
-            return Promise.all(pokemonArray.map(async (pokemon) => {
-                const response = await axios.get(pokemon.url);
-                return response.data;
-            }));
-        };
-
-        const matchedPokemon = searchTerm
-            ? allPokemon.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()))
-            : allPokemon;
-
-        const startIndex = currentPage * itemsPerPage;
-        const selectedPokemon = matchedPokemon.slice(startIndex, startIndex + itemsPerPage);
-
-        if (selectedPokemon.length) {
-            fetchPokemonDetails(selectedPokemon).then(setDisplayedPokemon);
-        } else {
-            setDisplayedPokemon([]);
-        }
-    }, [searchTerm, allPokemon, currentPage, itemsPerPage]);
-
-    useEffect(() => {
-        setCurrentPage(0);
-    }, [searchTerm]);
-
-    const handleNextPage = () => {
-        setCurrentPage(currentPage + 1);
+  useEffect(() => {
+    const fetchAllPokemon = async () => {
+      try {
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1000');
+        setAllPokemon(response.data.results);
+        setLoading(false)
+      } catch (error) {
+        console.error('Error fetching all pokemon:', error);
+      }
     };
 
-    const handlePrevPage = () => {
-        setCurrentPage(Math.max(0, currentPage - 1));
-    };
+    const loading = setTimeout(() => {
+      fetchAllPokemon();
+    }, 1500);
 
-    const totalResults = searchTerm
-        ? allPokemon.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())).length
-        : allPokemon.length;
-    const totalPages = Math.ceil(totalResults / itemsPerPage);
+    return () => clearTimeout(loading);
+  }, []);
 
-    if (loading) {
-        return <Loading />;
+  useEffect(() => {
+      const fetchPokemonDetails = async (pokemonArray) => {
+          try {
+              const promises = pokemonArray.map(async (pokemon) => {
+                  return axios.get(pokemon.url);
+              });
+
+              const responses = await Promise.all(promises);
+              return responses.map(response => response.data);
+          } catch (err) {
+              console.error('Erreur dans la requête:', err);
+              setLoading(false);
+              throw err;
+          }
+      };
+
+    const matchedPokemon = searchTerm
+      ? allPokemon.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()))
+      : allPokemon;
+
+    const startIndex = currentPage * itemsPerPage;
+    const selectedPokemon = matchedPokemon.slice(startIndex, startIndex + itemsPerPage);
+
+    if (selectedPokemon.length) {
+      fetchPokemonDetails(selectedPokemon).then(setDisplayedPokemon);
+    } else {
+      setDisplayedPokemon([]);
     }
+  }, [searchTerm, allPokemon, currentPage, itemsPerPage]);
 
-    const colours = {
-        normal: '#A8A77A',
-        fire: '#EE8130',
-        water: '#6390F0',
-        electric: '#F7D02C',
-        grass: '#7AC74C',
-        ice: '#96D9D6',
-        fighting: '#C22E28',
-        poison: '#A33EA1',
-        ground: '#E2BF65',
-        flying: '#A98FF3',
-        psychic: '#F95587',
-        bug: '#A6B91A',
-        rock: '#B6A136',
-        ghost: '#735797',
-        dragon: '#6F35FC',
-        dark: '#705746',
-        steel: '#B7B7CE',
-        fairy: '#D685AD',
+  useEffect(() => {
+    setCurrentPage(0);
+  }, [searchTerm]);
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage(Math.max(0, currentPage - 1));
+  };
+
+  const totalResults = searchTerm
+    ? allPokemon.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())).length
+    : allPokemon.length;
+  const totalPages = Math.ceil(totalResults / itemsPerPage);
+
+  if (loading) {
+    return <Loading/>;
+  }
+
+  const colours = {
+    normal: '#A8A77A',
+    fire: '#EE8130',
+    water: '#6390F0',
+    electric: '#F7D02C',
+    grass: '#7AC74C',
+    ice: '#96D9D6',
+    fighting: '#C22E28',
+    poison: '#A33EA1',
+    ground: '#E2BF65',
+    flying: '#A98FF3',
+    psychic: '#F95587',
+    bug: '#A6B91A',
+    rock: '#B6A136',
+    ghost: '#735797',
+    dragon: '#6F35FC',
+    dark: '#705746',
+    steel: '#B7B7CE',
+    fairy: '#D685AD',
+  };
+
+  const addToPokedex = (pokemon) => {
+    return () => {
+      const storagePokemon = localStorage.getItem('pokemon');
+      if (storagePokemon) {
+        let existingPokemon = JSON.parse(storagePokemon);
+        let pokemonExists = existingPokemon.some(objetPokemon => objetPokemon.id === pokemon.id);
+
+        if (!pokemonExists) {
+          existingPokemon.push(pokemon);
+          localStorage.setItem('pokemon', JSON.stringify(existingPokemon));
+          toast("Pokémon ajouté au pokédex");
+        } else {
+          toast("Ce pokémon est déjà dans votre pokédex");
+        }
+      } else {
+        localStorage.setItem('pokemon', JSON.stringify([pokemon]));
+        toast("Pokémon ajouté au pokédex");
+      }
     };
-
-    const addToPokedex = (pokemon) => {
-        return () => {
-            const storagePokemon = localStorage.getItem('pokemon');
-            if (storagePokemon) {
-                let existingPokemon = JSON.parse(storagePokemon);
-                let pokemonExists = existingPokemon.some(objetPokemon => objetPokemon.id === pokemon.id);
-
-                if (!pokemonExists) {
-                    existingPokemon.push(pokemon);
-                    localStorage.setItem('pokemon', JSON.stringify(existingPokemon));
-                    toast("Pokémon ajouté au pokédex");
-                } else {
-                    toast("Ce pokémon est déjà dans votre pokédex");
-                }
-            } else {
-                localStorage.setItem('pokemon', JSON.stringify([pokemon]));
-                toast("Pokémon ajouté au pokédex");
-            }
-        };
-    };
+  };
 
   return (
     <>
       <Toaster/>
-      <h1 style={{fontSize: '5vw', fontFamily: 'Pokemon Solid, sans-serif', marginBottom: '5%', color: '#ffcb05'}} className={"text-center"}>My Pokémon</h1>
+      <h1 style={{fontSize: '5vw', fontFamily: 'Pokemon Solid, sans-serif', marginBottom: '5%', color: '#ffcb05'}}
+          className={"text-center"}>My Pokémon</h1>
       <div className={"d-flex flex-column justify-content-center align-items-center gap-4"}>
         <div>
           <input
@@ -131,33 +139,43 @@ const PokemonList = () => {
             value={searchTerm}
             onChange={e => setSearchTerm(e.target.value)}
           />
-          </div>
-            <Link style={{width: "20vw", fontSize: "1vw"}} className={"btn btn-warning"} to={{pathname: "/my-pokedex"}} ><img className={"pe-3"} style={{minWidth: "50px", width: "20%"}} src={"https://www.freeiconspng.com/thumbs/pokeball-png/free-pokeball-download-3.png"} alt={"pokeball"}/>Accéder au pokédex personnel</Link>
+        </div>
+        <Link style={{width: "20vw", fontSize: "1vw"}} className={"btn btn-warning"} to={{pathname: "/my-pokedex"}}><img
+          className={"pe-3"} style={{minWidth: "50px", width: "20%"}}
+          src={"https://www.freeiconspng.com/thumbs/pokeball-png/free-pokeball-download-3.png"} alt={"pokeball"}/>Accéder
+          au pokédex personnel</Link>
         <div className={"d-flex gap-3 flex-wrap justify-content-center align-items-center"}>
-                    {displayedPokemon.map((pokemon, index) => (
-                        <div key={index} className={"card pokemon-dislike-button"} style={{ width: '18rem', backgroundColor: colours[pokemon.types[0].type.name] }}>
-                            <img className={"card-img-top"} src={pokemon.sprites.front_default} alt="Sprite pokémon" />
-                            <div className={"card-body d-flex flex-column align-items-center justify-content-center"}>
-                                <p className={"card-text"}>{pokemon.id} - {pokemon.name}</p>
-                                <p className={"card-text"}>Type(s): {pokemon.types.map(type => type.type.name).join(', ')}</p>
-                                <button type={'button'} className={"btn btn-success"} onClick={addToPokedex(pokemon)}>Ajouter au Pokédex</button>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-                {totalPages > 1 && (
-                    <div className="navigation-buttons">
-                        <button className={"position-fixed rounded-circle z-1 btn btn-secondary"} style={{ left: '2%', top: '50%', transform: 'translate(-50%, -50%)', width: '50px', height: '50px' }} onClick={handlePrevPage} disabled={currentPage === 0}>
-                            <FontAwesomeIcon icon={faChevronLeft} />
-                        </button>
-                        <button className={"position-fixed rounded-circle z-1 btn btn-secondary"} style={{ right: '2%', top: '50%', transform: 'translate(50%, -50%)', width: '50px', height: '50px' }} onClick={handleNextPage} disabled={currentPage >= totalPages - 1}>
-                            <FontAwesomeIcon icon={faChevronRight} />
-                        </button>
-                    </div>
-                )}
+          {displayedPokemon.map((pokemon, index) => (
+            <div key={index} className={"card pokemon-dislike-button"}
+                 style={{width: '18rem', backgroundColor: colours[pokemon.types[0].type.name]}}>
+              <img className={"card-img-top"} src={pokemon.sprites.front_default} alt="Sprite pokémon"/>
+              <div className={"card-body d-flex flex-column align-items-center justify-content-center"}>
+                <p className={"card-text"}>{pokemon.id} - {pokemon.name}</p>
+                <p className={"card-text"}>Type(s): {pokemon.types.map(type => type.type.name).join(', ')}</p>
+                <button type={'button'} className={"btn btn-success"} onClick={addToPokedex(pokemon)}>Ajouter au
+                  Pokédex
+                </button>
+              </div>
             </div>
-        </>
-    );
+          ))}
+        </div>
+        {totalPages > 1 && (
+          <div className="navigation-buttons">
+            <button className={"position-fixed rounded-circle z-1 btn btn-secondary"}
+                    style={{left: '2%', top: '50%', transform: 'translate(-50%, -50%)', width: '50px', height: '50px'}}
+                    onClick={handlePrevPage} disabled={currentPage === 0}>
+              <FontAwesomeIcon icon={faChevronLeft}/>
+            </button>
+            <button className={"position-fixed rounded-circle z-1 btn btn-secondary"}
+                    style={{right: '2%', top: '50%', transform: 'translate(50%, -50%)', width: '50px', height: '50px'}}
+                    onClick={handleNextPage} disabled={currentPage >= totalPages - 1}>
+              <FontAwesomeIcon icon={faChevronRight}/>
+            </button>
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default PokemonList;
